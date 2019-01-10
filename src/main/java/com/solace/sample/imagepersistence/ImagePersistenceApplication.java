716b99b812b58bf23d0886d5db5bcc21c3761b1a
Ush Shukla <21085773+Ushnash@users.noreply.github.com>
2019-01-10 17:49:27 -0600

    Code Cleanup This update is a general cleanup of the S3 code. The intention was to make the code easier to read & enable file logging for troubleshooting purposes.
    -Removed unused dependencies
    -Consolidated everything into 1 project
    -Correctly imported solace-java-spring-boot-starter
    -Updated Application.properties to use environment variables directly
    -General cleanup
    -Added Actuator support to permit app health checks

---
+++ src/main/java/com/solace/sample/imagepersistence/ImagePersistenceApplication.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.solace.sample.imagepersistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.solacesystems.jcsmp.ConsumerFlowProperties;
+import com.solacesystems.jcsmp.EndpointProperties;
+import com.solacesystems.jcsmp.FlowReceiver;
+import com.solacesystems.jcsmp.JCSMPFactory;
+import com.solacesystems.jcsmp.JCSMPProperties;
+import com.solacesystems.jcsmp.JCSMPSession;
+import com.solacesystems.jcsmp.Queue;
+import com.solacesystems.jcsmp.SpringJCSMPFactory;
+
+@SpringBootApplication
+public class ImagePersistenceApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ImagePersistenceApplication.class);
+
+    @Autowired
+    private SpringJCSMPFactory solaceFactory;
+
+    @Value("${app.queue}")
+    private String imageQueueName;
+
+    @Override
+    public void run(String... strings) throws Exception {
+	final JCSMPSession session = solaceFactory.createSession();
+
+	final EndpointProperties queueEndpointProps = new EndpointProperties();
+	// set queue permissions to "consume" and access-type to "exclusive"
+	queueEndpointProps.setPermission(EndpointProperties.PERMISSION_CONSUME);
+	queueEndpointProps.setAccessType(EndpointProperties.ACCESSTYPE_NONEXCLUSIVE);
+
+	// create the queue object locally
+	final Queue queue = JCSMPFactory.onlyInstance().createQueue(imageQueueName);
+
+	// Actually provision it, and do not fail if it already exists
+	session.provision(queue, queueEndpointProps, JCSMPSession.FLAG_IGNORE_ALREADY_EXISTS);
+
+	logger.info("Subscribed to queue {}", imageQueueName);
+
+	// Create a Flow be able to bind to and consume messages from the Queue.
+	final ConsumerFlowProperties flow_prop = new ConsumerFlowProperties();
+	flow_prop.setEndpoint(queue);
+	flow_prop.setAckMode(JCSMPProperties.SUPPORTED_MESSAGE_ACK_AUTO);
+
+	EndpointProperties consumerEndpointProps = new EndpointProperties();
+	consumerEndpointProps.setAccessType(EndpointProperties.ACCESSTYPE_NONEXCLUSIVE);
+
+	ImagePersistenceMessageConsumer msgConsumer = new ImagePersistenceMessageConsumer();
+	FlowReceiver cons = session.createFlow(msgConsumer, flow_prop, consumerEndpointProps);
+
+	logger.info("Connected. Awaiting message...");
+	cons.start();
+
+	// Consumer session is now hooked up and running!
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(ImagePersistenceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/solace/sample/imagepersistence/ImagePersistenceMessageConsumer.java
@@ -0,0 +1,225 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.solace.sample.imagepersistence;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.solacesystems.jcsmp.BytesXMLMessage;
+import com.solacesystems.jcsmp.JCSMPException;
+import com.solacesystems.jcsmp.TextMessage;
+import com.solacesystems.jcsmp.XMLMessageListener;
+
+import twitter4j.MediaEntity;
+import twitter4j.Status;
+
+public class ImagePersistenceMessageConsumer implements XMLMessageListener {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+    private static final Logger logger = LoggerFactory.getLogger(ImagePersistenceMessageConsumer.class);
+
+    @Value("${aws.clientregion}")
+    String clientRegion;
+
+    @Value("${aws.s3.bucket}")
+    String bucketName;
+
+    @Value("${aws.s3.objectkeyprefix}")
+    String objectKeyPrefix;
+
+    @Value("${app.image.source.default}")
+    String source;
+
+    // default constructor
+    public ImagePersistenceMessageConsumer() {
+    }
+
+    @Override
+    public void onReceive(BytesXMLMessage msg) {
+
+	String objKeyName = UUID.randomUUID().toString().replace("-", "") + ".jpg";
+
+	String filePath = null;
+	byte[] imageData = null;
+	boolean isJpeg = true;
+
+	if (msg instanceof TextMessage) {
+	    logger.info("============= TextMessage received: {} ", ((TextMessage) msg).getText());
+	} else {
+
+	    logger.info("============= Message received.");
+
+	    ByteBuffer binaryAttachment = msg.getAttachmentByteBuffer();
+
+	    logger.info("============= Checking if this message is a tweet");
+
+	    // See if the message is a tweet
+	    try {
+		byte[] tweetBytes = binaryAttachment.array();
+
+		Status tweet = (Status) convertFromBytes(tweetBytes);
+
+		source = tweet.getUser().getScreenName();
+		logger.info("Tweet received from {}", source);
+
+		filePath = getFilePath(objKeyName, source);
+
+		MediaEntity[] medias = tweet.getMediaEntities(); // get the media entities from the status
+
+		for (MediaEntity m : medias) {
+
+		    try {
+			URL url = new URL(m.getMediaURL());
+
+			InputStream in = new BufferedInputStream(url.openStream());
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024];
+			int n = 0;
+			while (-1 != (n = in.read(buf))) {
+			    out.write(buf, 0, n);
+			}
+			out.close();
+			in.close();
+
+			imageData = out.toByteArray();
+
+			if (!"jpg".equals(getExtension(m.getType()))) {
+			    isJpeg = false;
+			}
+
+		    } catch (Exception ex) {
+			ex.printStackTrace();
+		    }
+
+		    // only allowing one image per tweet
+		    break;
+		}
+	    } catch (Exception ex) {
+
+		// Treat binaryAttachment as an image and not a tweet
+		imageData = new byte[binaryAttachment.remaining()];
+		binaryAttachment.get(imageData);
+
+		filePath = getFilePath(objKeyName, source);
+
+	    }
+
+	    try {
+
+		logger.info("============= Not a tweet. Treating the message as a JPG image.");
+
+		if (isJpeg) {
+
+		    // Build an S3 Client to persist the image.
+		    // Passing null to the Credential manager uses the
+		    // DefaultAWSCredentialsProviderChain
+		    AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(clientRegion).withCredentials(null)
+			    .build();
+
+		    // Upload a file as a new object with ContentType and title specified.
+		    InputStream fileInputStream = new ByteArrayInputStream(imageData);
+		    ObjectMetadata metadata = new ObjectMetadata();
+		    metadata.setContentType("image/jpeg");
+		    metadata.addUserMetadata("x-amz-meta-title", objKeyName);
+		    metadata.setContentLength(imageData.length);
+
+		    PutObjectRequest request = new PutObjectRequest(bucketName, objectKeyPrefix + filePath,
+			    fileInputStream, metadata);
+		    s3Client.putObject(request);
+
+		    logger.info("============= Image persisted to file: {} ", filePath);
+		}
+	    } catch (AmazonServiceException e) {
+		// The call was transmitted successfully, but Amazon S3 couldn't process
+		// it, so it returned an error response.
+		e.printStackTrace();
+	    } catch (SdkClientException e) {
+		// Amazon S3 couldn't be contacted for a response, or the client
+		// couldn't parse the response from Amazon S3.
+		e.printStackTrace();
+	    }
+
+	}
+
+	latch.countDown(); // unblock main thread
+    }
+
+    private String getFilePath(String objKeyName, String source) {
+	String filePath;// Set filePath
+	filePath = source + "-" + objKeyName;
+	return filePath;
+    }
+
+    @Override
+    public void onException(JCSMPException e) {
+	logger.info("Consumer received exception:", e);
+	latch.countDown(); // unblock main thread
+    }
+
+    public void writeBuffer(ByteBuffer buffer, OutputStream stream) throws IOException {
+	WritableByteChannel channel = Channels.newChannel(stream);
+
+	channel.write(buffer);
+    }
+
+    private Object convertFromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+	try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes); ObjectInput in = new ObjectInputStream(bis)) {
+	    return in.readObject();
+	}
+    }
+
+    private String getExtension(String type) {
+	if (type.equals("photo")) {
+	    return "jpg";
+	} else if (type.equals("video")) {
+	    return "mp4";
+	} else if (type.equals("animated_gif")) {
+	    return "gif";
+	} else {
+	    return "err";
+	}
+    }
+
+    public CountDownLatch getLatch() {
+	return latch;
+    }
+
+}
